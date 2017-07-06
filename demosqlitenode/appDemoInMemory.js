9d657a79ea05b35555396325d3221efc731984d8
Pato <saurionx2005@gmail.com>
2017-07-06 14:49:14 -0300

    fix

--- demosqlitenode/appDemoInMemory.js
+++ demosqlitenode/appDemoInMemory.js
@@ -3,26 +3,28 @@ var app = express();
 
 app.get('/', function (req, res) {
   var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-
+//var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('blogNode');//creamos la base de datos llamada blogNode si no existe
 db.serialize(function() {
 
-  db.run('CREATE TABLE lorem (info TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS lorem (info TEXT)');
   var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
 
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 100; i++) {
     stmt.run('Ipsum ' + i);
   }
 
   stmt.finalize();
+  var listado = '';
 
   db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-    console.log(row.id + ': ' + row.info);
+    //console.log(row.id + ': ' + row.info);
+    listado = listado +row.info;
   });
 });
 
 db.close();
-  res.send('Hello World!');
+  res.send('Hello World!'+listado);
 
 });
 
